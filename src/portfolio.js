7f676922d2622f8c51e7164460c2d5110d168e34
Nick <nicholas.g.wright86@gmail.com>
2023-11-02 17:15:22 +0800

    Testing push changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Nicholas Wright",
-  title: "Hi all, I'm Nick",
+  title: "Hi all, I'm Nickster",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
