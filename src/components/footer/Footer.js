5e3f31674f1f0165bd5d30abd7003c29187a7909
Nick <nicholas.g.wright86@gmail.com>
2024-01-01 11:24:54 +0800

    Fine tuning padding and minor color fixes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,12 +10,8 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
+        <a href="https://github.com/saadpasta/developerFolio">
+          {emoji("Adapted 🔌 from Developer Folio Template ⭐")}
           </a>
         </p>
       </div>
